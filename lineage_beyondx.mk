cd94aa570ba61656ffffd6e3d33f3457a0485b04
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    beyondx: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- lineage_beyondx.mk
+++ lineage_beyondx.mk
@@ -42,9 +42,6 @@ MATRIXX_CHIPSET := exynos9820
 MATRIXX_BATTERY := 4500mah
 MATRIXX_DISPLAY := 1440x3040
 
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_beyondx
 PRODUCT_DEVICE := beyondx
